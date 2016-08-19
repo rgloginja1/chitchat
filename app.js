29f499d43211f39bf5aecdb002f1dd18aca4cdc8
RMG <robertgloginja@gmail.com>
2016-08-19 23:25:03 +1000

    Update app.js
    Fixed user tagging break

--- app.js
+++ app.js
@@ -173,7 +173,7 @@ io.sockets.on('connection', function (socket) {
 					return this.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
 						var username = u.replace("@","")
 						//return u.link("http://twitter.com/"+username);
-						return '<a href="#" onclick="hello(this);">@' + username + '</a>';
+						return '<a href="#" onclick="hello(this);">@'+ username + '</a>';
 					});
 				}; 
 
